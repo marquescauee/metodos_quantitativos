7b1cbbd44756d887f9fa658d9e1af71d5290c504
marquescauee <cauelopesmarques123@gmail.com>
2023-06-18 17:36:01 -0300

    README Annotations

---
+++ greedy_algorithm.py
@@ -0,0 +1,51 @@
+import random
+
+for i in range(1, 30):
+
+    #Função que gera o grafo
+    def build_graph_from_file(file_path):
+        graph = {}
+
+        with open(file_path, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    vertex1, vertex2 = map(int, line.split())
+                    if vertex1 not in graph:
+                        graph[vertex1] = []
+                    if vertex2 not in graph:
+                        graph[vertex2] = []
+                    graph[vertex1].append(vertex2)
+                    graph[vertex2].append(vertex1)
+        return graph
+
+    file_path = './'+str(i)+'/result'+str(i)+'.txt'
+    graph = build_graph_from_file(file_path)
+
+
+    #Algoritmo Guloso
+    def greedy_maximal_independent_set(graph, max_iterations):
+        best_solution = set()
+        iterations = 0
+
+        while iterations < max_iterations:
+            current_solution = set()
+            vertices = list(graph.keys())
+            random.shuffle(vertices)
+
+            for vertex in vertices:
+                if all(neighbour not in current_solution for neighbour in graph[vertex]):
+                    current_solution.add(vertex)
+
+            if len(current_solution) > len(best_solution):
+                best_solution = current_solution
+
+            iterations += 1
+
+        return best_solution
+
+    #Número de iterações
+    max_iterations = 1000
+
+    solution = greedy_maximal_independent_set(graph, max_iterations)
+    print(f"Best Solution for Instance {i} After {max_iterations} iterations: {len(solution)}. Vertices Selected: {solution} \n")
