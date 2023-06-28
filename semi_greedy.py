102155304ebf67e586f875657417fb687856e3be
marquescauee <cauelopesmarques123@gmail.com>
2023-06-27 22:17:54 -0300

    Semi-Greedy Comments

--- semi_greedy.py
+++ semi_greedy.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 #Selecionando apenas as instâncias ímpares (step 2)
@@ -20,39 +21,64 @@ for i in range(1, 30, 2):
         return graph
 
     file_path = './'+str(i)+'/result'+str(i)+'.txt'
+    graph = build_graph_from_file(file_path)
   
     #Algoritmo Semi-Greedy
     def semi_greedy(iterations, k):
-        graph = build_graph_from_file(file_path)
+        #variáveis que serão usadas para comparação no final da iteração
         best_independent_set = set()
         best_independent_size = 0 
+
+        #ordenando o grafo do vértice de menor ordem para o de maior
+        #forma de ordenação: função lambda recebe um parâmetro x (chave/vértice do dicionário/grafo) e retorna o comprimento do array de valores
         sorted_candidates = sorted(graph.items(), key=lambda x: len(x[1]))
+
+        #convertendo a lista ordenada para um grafo novamente
         sorted_candidates = dict(sorted_candidates)
+
+        #inicio das iterações
         for _ in range(iterations):
+            #cópia do grafo (necessário reiniciar o grafo a cada nova iteração/tentativa)
+            new_graph_instance = copy.deepcopy(graph)
+
+            #cópia da lista ordenada de candidatos em forma de lista (necessário para não apontarem para o mesmo endereço de memória)
             candidates = list(sorted_candidates)
-            independent_set = set()
+
+            #melhor conjunto independente máximo atual
+            current_independent_set = set()
+
+            #enquanto houver candidatos, seleciona uma porcentagem dos candidatos e reatribui ao array original
             while candidates:
+
+                #definição da quantidade de candidatos a serem reatribuídos no array
                 partial_candidates = int(len(candidates) * (k/100) + 1)
+
+                #reatribuição do array
                 candidates = candidates[:partial_candidates]
 
+                #seleção aleatória do percentual de candidatos
                 chosen_vertice = random.choice(candidates)
 
-                independent_set.add(chosen_vertice)
+                #adiciona o vertice escolhido ao conjunto independente máximo atual
+                current_independent_set.add(chosen_vertice)
+
+                #remove o vértice escolhido do conjunto de candidatos
                 candidates.remove(chosen_vertice)
                 
-                for neighbor in graph[chosen_vertice]:
+                #cada vizinho (valor) do vértice escolhido (chave) também é removido da lista de candidatos
+                for neighbor in new_graph_instance[chosen_vertice]:
                     if neighbor in candidates:
                         candidates.remove(neighbor) 
                
-
-            independent_size = len(independent_set)
+            #se o tamanho do conjunto independente máximo atual é maior que o melhor encontrado até o momento, substitui
+            independent_size = len(current_independent_set)
             if independent_size > best_independent_size:
-                best_independent_set =  independent_set
+                best_independent_set =  current_independent_set
                 best_independent_size = independent_size
         return best_independent_set
 
     #Número de iterações
-    max_iterations = 10000
+    max_iterations = 1000
 
     #Porcentagem de candidatos
     k = 100
